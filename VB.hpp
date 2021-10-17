1a9ee2595f347261239126cecfc730f1f4113884
HeliumHe-He <bartol.hrg.backup@gmail.com>
2021-10-17 21:46:53 +0200

    VB.HPP initial commit

---
+++ VB.hpp
@@ -0,0 +1,36 @@
+#ifndef VB_HPP
+#define VB_HPP
+
+#include "VB.h"
+
+#undef STRUCT
+#define STRUCT(name) struct name {
+
+#undef ENUM
+#define ENUM(name) enum name {
+    
+#define CLASS(name) class name {
+#define ENDCLASS };
+
+#define NAMESPACE namespace
+#define STARTNAMESPACE {
+#define ENDNAMESPACE }
+
+#define USING using
+
+#define TEMPLATE template
+#define TYPENAME typename
+
+#define PRIVATE private
+#define PUBLIC public
+#define PROTECTED protected
+
+#define FRIEND friend
+
+#define STATIC static
+
+#define OPERATOR operator 
+
+#define DEFAULT default
+
+#endif // VB_HPP
